367d6df46a654a09393e363e7846692b6539e70a
Elodie Riou <elodie.riou@hotmail.com>
2022-12-12 20:31:31 +0100

    FEATURE: parsing Headings Markdown syntax for generating HTML

--- markdown2html.py
+++ markdown2html.py
@@ -6,14 +6,23 @@ if __name__ == "__main__":
     from sys import argv, stderr
     from os.path import exists
 
-    filename = "README.md"
+    markdownFile = "README.md"
+    htmlFile = "README.html"
 
     if len(argv) <= 2:
         stderr.write("Usage: ./markdown2html.py README.md README.html \n")
         exit(1)
 
-    if not exists(filename):
-        print("Missing {}".format(filename), file=stderr)
+    if not exists(markdownFile):
+        print("Missing {}".format(markdownFile), file=stderr)
         exit(1)
-    else:
-        exit(0)
+
+    with open(markdownFile, 'r',encoding='utf-8') as markdown:
+        with open(htmlFile, 'w', encoding='utf-8') as html:
+            count = 1
+            for line in markdown:
+                heading = line.strip().count('#')
+                title = line.lstrip('# ').rstrip("\n")
+                html.write("<h{}>{}</h{}>\n".format(heading, title, heading))
+                count += 1
+    exit(0)
