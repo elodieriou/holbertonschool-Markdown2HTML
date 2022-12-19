9960e252440d138c60ea2a72ac2040f85dadbdc1
Elodie Riou <elodie.riou@hotmail.com>
2022-12-19 18:00:34 +0100

    FIX: match any character zero or more times

--- markdown2html.py
+++ markdown2html.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
                 line = line.replace("__", "</em>", 1)
 
                 # Converting in MD5
-                findRegex = re.findall(r'\[\[(.+)\]\]', line)
+                findRegex = re.findall(r'\[\[(.*)\]\]', line)
                 print(findRegex)
                 if findRegex:
                     line = line\
@@ -47,7 +47,7 @@ if __name__ == "__main__":
                         .translate({ord(i): None for i in '[]'})
 
                 # Removing '(', ')' 'C', 'c'
-                findRegex2 = re.findall(r'\(\((.+)\)\)', line)
+                findRegex2 = re.findall(r'\(\(.*\)\)', line)
                 print(findRegex2)
                 if findRegex2:
                     line = line.translate({ord(i): None for i in '()Cc'})
